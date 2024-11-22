4215161c675254c40782b0df645ee6f1155f28b7
Hiep Nguyen <hiepnv.wt@gmail.com>
2024-11-22 09:05:40 +0700

    fix: update shadcn component json config

--- src/modules/shared/componenets/ui/dialog.tsx
+++ src/modules/shared/componenets/ui/dialog.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
 
-import { cn } from '~/modules/shared/lib/tailwind-clsx'
+import { cn } from '~/modules/shared/lib/tailwind-cn'
 
 const Dialog = DialogPrimitive.Root
 
