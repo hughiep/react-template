fbd036b25b5e5035022ce2e0b7901ffc1d977d6a
Hiep Nguyen <hiepnv.wt@gmaill.com>
2025-06-20 17:28:33 +0700

    refactor: remove tailwind classes

--- src/router/not-found.tsx
+++ src/router/not-found.tsx
@@ -1,17 +1,12 @@
 export default function NotFoundPage() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4 text-center">
+    <div>
       <div>
-        <h1 className="text-9xl font-extrabold text-cyan-950">404</h1>
+        <h1>404</h1>
         <p>Page Not Found</p>
       </div>
 
-      <button
-        onClick={() => (window.location.href = '/')}
-        className="underline"
-      >
-        Go to Home
-      </button>
+      <button onClick={() => (window.location.href = '/')}>Go to Home</button>
     </div>
   )
 }
