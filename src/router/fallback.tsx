4fc33ea9bb53f68b1587630eabf74483e6b2ae45
Hiep Nguyen <hiepnv.wt@gmail.com>
2025-08-09 14:08:41 +0700

    refactor: reorganize imports in router.tsx for improved readability; update fallback message in fallback.tsx

--- src/router/fallback.tsx
+++ src/router/fallback.tsx
@@ -2,5 +2,5 @@
 // Replacement for the fallbackElement, which is depricated in v7
 // Useful in case our loaders take long time to resolve
 export default function FallbackElement() {
-  return <div>Loading...</div>
+  return <div>Fallback...</div>
 }
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,17 +1,16 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import type { RouteObject } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import RootLayout from '@/shared/layouts/root'
 
-import { moduleRoutes } from './routes'
-import PrivateRoute from './private-route'
-import NotFoundPage from './not-found'
 import ErrorBoundary from './error'
+import NotFoundPage from './not-found'
+import PrivateRoute from './private-route'
+import { moduleRoutes } from './routes'
 
 export const Router = () => {
   const appRoutes: RouteObject[] = [
     {
-      path: '/',
       element: <RootLayout />,
       errorElement: <ErrorBoundary />,
       children: [
