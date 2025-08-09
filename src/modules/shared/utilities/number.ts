b77b3940f306d67fc0cb41a5f7b355c3f5d8d2f9
Hiep Nguyen <hiepnv.wt@gmail.com>
2025-08-09 14:12:48 +0700

    chore: refactor structure

--- src/modules/shared/utilities/number.ts
+++ src/modules/shared/utilities/number.ts
@@ -3,3 +3,6 @@
  * This module provides functions to format numbers, check if a number is even or odd,
  * and convert numbers to strings with specific formatting.
  */
+export const formatNumber = (num: number | `${number}`): string => {
+  return new Intl.NumberFormat().format(Number(num))
+}
